8155f1fedfe07833e6ef6f3ce1224945a28256f8
FotoDave <davewallacefotofoto@gmail.com>
2022-09-06 09:36:54 +0100

    Commit du 6 septembre 2022

--- src/main/java/org/work/ebankbackend/repositories/CustomerRepository.java
+++ src/main/java/org/work/ebankbackend/repositories/CustomerRepository.java
@@ -1,7 +1,13 @@
 package org.work.ebankbackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.work.ebankbackend.entities.Customer;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    @Query("select c from Customer c where c.name like :kw")
+    List<Customer> searchCustomer(@Param("kw") String keyword);
 }
